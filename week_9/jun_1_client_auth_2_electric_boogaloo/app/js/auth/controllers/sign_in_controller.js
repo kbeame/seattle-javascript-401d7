7a0f9e009d38a477d6978ad6556fcab5723b31b4
Katherine Beame <katherine.beame@gmail.com>
2016-06-04 22:02:28 -0700

    forgot to commit this change

--- week_9/jun_1_client_auth_2_electric_boogaloo/app/js/auth/controllers/sign_in_controller.js
+++ week_9/jun_1_client_auth_2_electric_boogaloo/app/js/auth/controllers/sign_in_controller.js
@@ -2,7 +2,7 @@ var baseUrl = require('../../config').baseUrl;
 module.exports = function(app) {
   app.controller('SignInController', ['$http', '$location', 'cfHandleError', 'cfAuth', function($http, $location, handleError, auth) {
     // AUTH_EX: how does this differ from the sign_up_controller?
-    
+    // NOTE These are combined explanations for sign-up and sign-in and thus are same for each question.
     // Sign-up: What is does generally: It sets sign-up to be true, and posts to the signup route created on the backend. It then saves part of the token response with the cfAuth service and AuthController.
     // Sign-in: Retrieves the content of a user’s credentials (aka token) and then in the same way as signing up it saves the responses within the cfAuth service in the AuthController.
     // The main differences between these two is that: sign-in can occur only when sign-up it equal to true.
